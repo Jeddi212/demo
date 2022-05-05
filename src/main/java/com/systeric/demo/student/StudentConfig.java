fbead55bbde7357bb9b321cc1c521d4fdae579b4
Jeddi212 <jedediahf51@gmail.com>
2022-05-06 01:43:51 +0700

    Get Api ↘

---
+++ src/main/java/com/systeric/demo/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.systeric.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.FEBRUARY;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student jeddi = new Student(
+                    "Jeddi",
+                    "jeddi@gmail.com",
+                    LocalDate.of(2001, FEBRUARY, 28),
+                    21
+            );
+
+            Student fanuel = new Student(
+                    "Fanuel",
+                    "fan@gmail.com",
+                    LocalDate.of(2004, FEBRUARY, 29),
+                    17
+            );
+
+            repository.saveAll(
+                    List.of(jeddi, fanuel)
+            );
+        };
+    }
+}
